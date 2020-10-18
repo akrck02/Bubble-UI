98a4e71be1fdfd7344aa7a6aebcaa72cbb4948de
akrck02 <akrck02@gmail.com>
2020-10-18 19:55:35 +0200

    Reestructure & mobile view improving

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,46 @@
+let mobile;
 let navBar_open;
 let navBar;
 
 //load
 function load(){
+    setMobile();
+    window.onresize = setMobile;
     navBar_open = false;
     getUI();
 }
+
+//set if small device
+function setMobile(){mobile = getSmallDevice();}
 
 //get UI elements
 function getUI(){
     navBar = document.querySelector("#navBar");
 }
 
+//get small devices
+function getSmallDevice(){
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    let small = window.innerWidth < 700;
+    let mobile = toMatch.some((toMatchItem) => {return navigator.userAgent.match(toMatchItem);});
+    return mobile || small;
+}
+
+
+
 //updateNavbar
 function updateNavbar(){
     if(!navBar_open) openNavBar();
     else closeNavBar();
+   
 }
 
 //open navbar
@@ -26,6 +51,8 @@ function openNavBar(){
 
 //close navbar
 function closeNavBar(){
-    navBar.style.left = '-400px';
+    if(mobile) navBar.style.left = "-100%";
+    else  navBar.style.left = '-400px';
+
     navBar_open = false;
 }
