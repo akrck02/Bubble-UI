bd4f408504a9c265968f8fd2d15dd5a303a897ea
akrck02 <akrck02@gmail.com>
2020-10-20 09:36:06 +0200

    atomic design

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,53 @@
 let mobile;
+let html;
 let navBar_open;
 let navBar;
 
 //load
 function load(){
+    getUI();
+    setThemeByTime();
     setMobile();
     window.onresize = setMobile;
     navBar_open = false;
-    getUI();
 }
 
 //set if small device
 function setMobile(){mobile = getSmallDevice();}
 
+//set theme
+function setThemeByTime(){
+    let d = new Date();
+    if(d.getHours() < 22) setLightTheme();
+    else setDarkTheme();
+}
+
+function changeTheme(){
+    if(html.getAttribute("data-theme") == "light") setDarkTheme();
+    else setLightTheme();
+}
+
+function setDarkTheme(){
+    html.setAttribute("data-theme", "dark");
+    let images = document.querySelectorAll(".themeImg");
+    images.forEach(img => {
+        img.setAttribute("src","img/dark-theme/" + img.getAttribute("data-src"));
+    });
+}
+
+function setLightTheme(){
+    html.setAttribute("data-theme", "light");
+    let images = document.querySelectorAll(".themeImg");
+    images.forEach(img => {
+        img.setAttribute("src","img/light-theme/" + img.getAttribute("data-src"));
+    });
+}
+
+
 //get UI elements
 function getUI(){
     navBar = document.querySelector("#navBar");
+    html = document.querySelector("html");
 }
 
 //get small devices
@@ -34,13 +66,10 @@ function getSmallDevice(){
     return mobile || small;
 }
 
-
-
 //updateNavbar
 function updateNavbar(){
     if(!navBar_open) openNavBar();
-    else closeNavBar();
-   
+    else closeNavBar();   
 }
 
 //open navbar
